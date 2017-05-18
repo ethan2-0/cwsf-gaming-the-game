b8d4a655bed48f4660ad7b604d732fd7ebdaa804
Atif-Mahmud <atifmahmud101@gmail.com>
2017-05-18 01:04:36 -0400

    Changes for Advait

--- ModListGenerator.py
+++ ModListGenerator.py
@@ -1,13 +1,13 @@
 words = []
 with open("words2.txt", "r") as f:
     words = [x.replace("\n", "") for x in f.readlines()]
-fl = open('./output_3mod4.txt', 'a');
+fl = open('./output_2mod4.txt', 'a');
 
-i = 3
+i = 2
 mod = 4
 le = len(words)
 
 while(i < le):
     for j in range (100):
-        fl.write(words[i] + str(j) + '\r\n')
+        fl.write(words[i] + str(j) + '\n')
     i += mod
